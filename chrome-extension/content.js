4689d98b44ce5094a44671057dda29dab94964aa
Owliie <sladurcheto5232@abv.bg>
2020-10-02 23:54:04 +0300

    Fixed content.js

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,5 +1,31 @@
+(() => {
+	const getArticleText = () => {
+		let articleContent;
+		const paragraphs = document.querySelectorAll("p");
 
+		for (const paragraph of paragraphs) {
+			articleContent += paragraph.textContent;
+		}
+
+		return articleContent;
+	};
+
+    chrome.storage.sync.get('analyze', (data) => {
+		if (data.analyze) {
+			const article = getArticleText();
+
+			alert(article);
 		}
 	});
 })();
 
+postContent = (content) => {
+	fetch('83.228.90.116:80/detectpropaganda', {
+		method: 'post',
+		body: content
+	}).then(function(response) {
+		return response.json();
+	}).then(function(data) {
+		alert(data);
+	});
+}
