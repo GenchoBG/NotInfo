ecf72d91deb7c0ecc5d46d85fa569edf59127ec9
Owliie <sladurcheto5232@abv.bg>
2020-10-02 23:03:04 +0300

    Added content script Fixed analyzer value storing on popup close

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -3,3 +3,17 @@ chrome.runtime.onInstalled.addListener(function() {
 		console.log('set analyzer to false');
 	});
 });
+
+// chrome.storage.onChanged.addListener(function(changes, namespace) {
+    
+// });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.method == "getAnalyzerStatus"){
+    	let analyzeStatus = false;
+    	chrome.storage.sync.get('analyze', function(data) {
+			analyzeStatus = data.analyze;
+		});
+      sendResponse({status: analyzeStatus});
+    }
+});
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,6 +1,19 @@
-  let checkbox = document.getElementById('analyzerSwitch');
+const checkbox = document.getElementById('analyzerSwitch');
 
-  chrome.storage.sync.get('analyze', function(data) {
-    checkbox.checked = data.analyze;
-    console.log(data)
-  });
+checkbox.addEventListener("click", function() {
+	const value = checkbox.checked;
+	chrome.storage.sync.set({'analyze': value});
+	if(value){
+		chrome.tabs.getSelected(null, function(tab) {
+		  	var code = 'window.location.reload();';
+		  	chrome.tabs.executeScript(tab.id, {code: code});
+		});
+	}
+});
+
+(function () {
+chrome.storage.sync.get(['analyze'], function(data) {
+	console.log(data)
+	checkbox.checked = data.analyze;
+});
+})();
